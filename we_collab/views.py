9982239d2fded3d360fcaf55c17d69b742ac103e
Giovanni Toffoli <toffoli@linkroma.it>
2022-04-02 15:07:51 +0200

    Show cover page only to anonymous users.

--- we_collab/views.py
+++ we_collab/views.py
@@ -1,6 +1,12 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.flatpages.models import FlatPage
 
-def home(request):
-    # return HttpResponseRedirect('/project/we-collab/')
+def cover(request):
     return render(request, 'flatpages/splash.html', {'flatpage': FlatPage.objects.get(url='/we-collab/cover/')})
+
+def home(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/project/we-collab/')
+    else:
+        return cover(request)
